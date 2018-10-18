a33e358e991d8b1d5253c7afc7a220e8ee764cfc
Jesper Kragh Jørgensen <jejoe16@student.sdu.dk>
2018-10-18 17:24:57 +0200

    basic components added

--- TrustPilot/src/app/app.module.ts
+++ TrustPilot/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BodyLayoutComponent } from './body-layout/body-layout.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyLayoutComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule
