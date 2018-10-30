78bde3c0c4cbe76df9c842e044d8e47b90614f24
Jesper Kragh Jørgensen <jejoe16@student.sdu.dk>
2018-10-30 16:00:05 +0100

    Added interface

---
+++ TrustPilot/src/app/DBServices/add-review-interface.ts
@@ -0,0 +1,16 @@
+export interface AddReviewInterface {
+}
+
+/**
+ * Method to save the review submitted by the user in the database.
+ * @param username
+ * @param rating
+ * @param title
+ * @param reviewText
+ * @param Company
+ * @constructor
+ */
+function SaveReview(username: String, rating: number, title: String, reviewText: String, Company: String){
+
+}
+
