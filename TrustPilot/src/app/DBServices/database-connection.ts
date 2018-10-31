6944059dcd79c505e6c530dbee68f4d990f5e555
Zunac <tobias_dahl@yahoo.dk>
2018-10-31 13:43:30 +0100

    Updated database-connection

--- TrustPilot/src/app/DBServices/database-connection.ts
+++ TrustPilot/src/app/DBServices/database-connection.ts
@@ -1,2 +1,15 @@
-export class DatabaseConnection {
+import {AddReviewInterface} from "../DBServices/add-review-interface";
+
+export class DatabaseConnection implements AddReviewInterface{
+  username: string;
+  rating: number;
+  title: String;
+
+   SaveReview(username: string, rating: number, title: string, reviewText: string, Company: string){
+    console.log(username);
+  }
+
+
 }
+
+
