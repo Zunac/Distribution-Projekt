81033965a699013ff392c33c1be22728c596017d
Marc226 <Marc.g.nielsen@gmail.com>
2018-11-09 12:55:07 +0100

    small changes

--- TrustPilot/src/app/review-list/review-list.component.ts
+++ TrustPilot/src/app/review-list/review-list.component.ts
@@ -29,6 +29,8 @@ export class ReviewListComponent implements OnInit {
 
     this.reviews = this.compService.getReviews(this.company_name);
 
+    document.getElementById("title-id").innerText = this.company_name;
+
   }
 
 
